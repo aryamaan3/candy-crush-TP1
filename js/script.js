1f13f8184b15ba143a62852e81f7d58b2c4aa224
Aryamaan Kunwar <aryamaan3@gmail.com>
2021-01-21 16:14:58 +0100

    added timer and css mod

--- js/script.js
+++ js/script.js
@@ -16,4 +16,21 @@ function init() {
   grille.showCookies();
 }
 
+//https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
+let sec = 0;
+
+function pad ( val ) { return val > 9 ? val : "0" + val; }
+setInterval( function(){
+  $("#seconds").html(pad(++sec%60));
+  $("#minutes").html(pad(parseInt(sec/60,10)));
+}, 1000);
+
+function pad(val) {
+  let valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
 
